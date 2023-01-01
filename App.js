433e4b48af33dda82085e852e768737a92de7d72
rohan760 <coderrohan1234@gmail.com>
2023-01-01 11:02:26 +0530

    code splitting

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", { id: "title" }, "heading 1");
+
+      const heading2 = React.createElement("h2", { id: "title" }, "heading 2");
+
+      const container = React.createElement("div", { id: "container" }, [
+        heading,
+        heading2,
+      ]);
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+      root.render(container);

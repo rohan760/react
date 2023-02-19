fb0e953c4a50eb4ec530f11090194ae424c79fa7
rohan760 <coderrohan1234@gmail.com>
2023-02-19 20:56:55 +0530

    lecture 2 finished

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Warning: You are importing createRoot from "react-dom" which is not supported. 
+// You should instead import it from "react-dom/client".
+
 const heading = React.createElement("h1", { id: "title" }, "heading 1");
 
       const heading2 = React.createElement("h2", { id: "title" }, "heading 2");
